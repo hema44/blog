df93b43d62301e4a804101dcacdb11efd8e016a3
ibrahem allam <heam897@gmail.com>
2021-03-23 19:39:46 +0200

    last update for api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentsController;
@@ -16,7 +15,6 @@ use App\Http\Controllers\PostsController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::group([
     'prefix' => 'auth'
 ], function () {
@@ -27,11 +25,14 @@ Route::group([
 
 
 Route::group(['middleware' => ['auth:api']], function() {
-    Route::post('posts/', [PostsController::class , 'store']);
-    Route::get('posts/',[PostsController::class , 'index']);
+
+    Route::post('post/', [PostsController::class , 'store']);
+    Route::get('post/',[PostsController::class , 'index']);
     Route::get('post/{id}',[PostsController::class , 'show']);
     Route::delete('post/{id}', [PostsController::class , 'destroy']);
     Route::put('post/{id}',[PostsController::class , 'update']);
+
+
     Route::post('comment/', [CommentsController::class , 'store']);
     Route::get('comment/',[CommentsController::class , 'index']);
     Route::get('comment/{id}',[CommentsController::class , 'show']);
