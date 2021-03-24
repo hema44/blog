3387063e6510002b0d749ddff77d49db1bd1a555
ibrahem allam <heam897@gmail.com>
2021-03-24 13:39:13 +0200

    update the UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,26 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserRequest;
+use App\Http\Requests\user\CreateUserRequest;
 use App\Models\User;
 
 
 class UserController extends Controller
 {
 
-    // function will store data to user table
-    public function store(UserRequest $request){
-        User::create([
-            'email' => $request->email,
-             'name' => $request->name,
-            'password' => bcrypt($request->password)
-        ]);
+    /**
+     *
+     *take data from request and validated it then store it into user table
+     *
+     * @author ibrahem
+     */
+
+    public function store(CreateUserRequest $request){
+        $data = $request->validate($request->rules());
+        $data['password'] = bcrypt($data['password'] );
+        User::create($data);
     }
-//this function will retreie all user data
+    /**
+     *
+     * @return JsonResponse for all user data
+     *
+     * @author ibrahem
+     */
+
     public function index(){
         return User::all();
     }
-    //this function will delete user by useing user_id
+
+    /**
+     *
+     * Get user_id to delete the user and all has posts and comments
+     *
+     * @author ibrahem
+     */
+
     public function destroy($id){
         User::where('id',$id)
         ->delete();
