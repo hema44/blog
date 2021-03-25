ee658331bf37cbcebefc5bfb342c0875050a8988
ibrahem allam <heam897@gmail.com>
2021-03-25 11:15:55 +0200

    adding the welcome mail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\user\CreateUserRequest;
+use Illuminate\Support\Facades\Mail;
 use App\Models\User;
+
 
 
 class UserController extends Controller
@@ -22,13 +24,37 @@ class UserController extends Controller
         $data['image'] = $image;
         $data['password'] = bcrypt($data['password'] );
         $data += ['created_at' => now()];
+        $this->sendmail($data['email']);
         User::insert($data);
     }
+    /**
+     *
+     * @return image_name after uploaded it in  public/images/user/
+     *
+     * @author ibrahem
+     */
     public function storeimage(){
         $post_image = $_FILES['image']['name'];
         $post_image_temp = $_FILES['image']['tmp_name'];
         move_uploaded_file($post_image_temp,"images/user/$post_image");
         return $post_image;
+    }
+    /**
+     *
+     * sending email with done
+     *
+     * @author ibrahem
+     */
+    public function sendmail($email){
+        $details = [
+          'name'=> 'successfully login in HOVO ',
+          'email' => $email
+        ];
+        Mail::send('welcome_email', $details, function ($message) use ($details) {
+            $message->to($details['email'], $details['name'])
+                ->subject('Thanks for be one of Hovo family')
+                ->from('info@mynotepaper.com', 'MyNotePaper');
+        });
     }
     /**
      *
